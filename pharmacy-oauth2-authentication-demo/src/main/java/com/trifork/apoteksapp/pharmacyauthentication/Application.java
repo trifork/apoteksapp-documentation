9cdf82a7dc5cf0e060266abc5ff5db4e503e6ee5
Henrik Herbst Sørensen <hhs@trifork.com>
2020-10-21 15:28:44 +0200

    Update cpr and pharmacyNumber to correspond to magic user and pharmacy

--- pharmacy-oauth2-authentication-demo/src/main/java/com/trifork/apoteksapp/pharmacyauthentication/Application.java
+++ pharmacy-oauth2-authentication-demo/src/main/java/com/trifork/apoteksapp/pharmacyauthentication/Application.java
@@ -49,7 +49,7 @@ public class Application {
 
         // ASP Pharmacy API configuration
         final String cpr = "0101010101";
-        final String pharmacyNumber = "003";
+        final String pharmacyNumber = "001-01";
         final String pharmacyApiEndpoint = "https://test1.apoteksapp.dk/asp/pharmacy/api/v1/pharmacy/" + pharmacyNumber + "/getuserinfo";
 
         // Load certificate and private key
